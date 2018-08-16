c0526df16861b4ab418e3c872bd8b0832cea2075
Joost van der Drift <djiest@gmail.com>
2018-08-16 09:29:35 +0200

    Updated jQuery UI - v1.12.1
    Updated jQuery UI - v1.12.1, because it was missing the jquery.tabs() functionality, which caused errors in the admin > forms functionality

--- admin/assets/js/jquery/jquery-ui.js
+++ admin/assets/js/jquery/jquery-ui.js
@@ -1,4 +1,4 @@
-/*! jQuery UI - v1.12.1 - 2018-07-26
+/*! jQuery UI - v1.12.1 - 2018-08-15
 * http://jqueryui.com
 * Includes: widget.js, position.js, data.js, disable-selection.js, focusable.js, form-reset-mixin.js, keycode.js, labels.js, scroll-parent.js, tabbable.js, unique-id.js, widgets/draggable.js, widgets/droppable.js, widgets/resizable.js, widgets/selectable.js, widgets/sortable.js, widgets/autocomplete.js, widgets/button.js, widgets/checkboxradio.js, widgets/controlgroup.js, widgets/datepicker.js, widgets/dialog.js, widgets/menu.js, widgets/mouse.js, widgets/tabs.js
 * Copyright jQuery Foundation and other contributors; Licensed MIT */
